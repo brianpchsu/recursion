e89fbcdb51d1638e48524dcc1b40bc59966dc889
Brian Hsu <brianhsu@brians-mbp.local.tld>
2015-01-19 01:26:54 -0500

    Finished getElementsByClassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+//I don't know could I change the parameter of the functions. if not, then I will need to 
+//create another function inside getElementByClassName
+var getElementsByClassName = function(className, currentNode, output){
+	// your code here
+	currentNode = currentNode || document.body;	//1st step is document.body, then the childNode
+	output = output || [];	//Original output is empty array
+
+	if (currentNode.classList && currentNode.classList.contains(className)){
+		output.push(currentNode);
+	}
+
+	var child = currentNode.childNodes;
+	for (var i = 0; i<child.length; i++) {
+		getElementsByClassName(className, child[i], output);
+	}
+	return output;
+
 };
